1857f730690d75089a4fbe184c148dca88080445
ilkyazarabaci <ilkyaz.arabaci@adesso.com.tr>
2023-10-02 21:24:01 +0300

    feat: update build script with compile method

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -2,18 +2,20 @@ const Fs = require('fs')
 const Path = require('path')
 const Sass = require('node-sass')
 
-const result = Sass.renderSync({
-    data: Fs.readFileSync(
-        Path.resolve('src/global.scss')
-    ).toString(),
-    outputStyle: 'expanded',
-    outFile: 'global.css',
-    includePaths: [Path.resolve('src')]
-})
+const compile = (path, fileName) => {
+    const result = Sass.renderSync({
+        data: Fs.readFileSync(
+            Path.resolve(path) // 'src/global.scss'
+        ).toString(),
+        outputStyle: 'expanded',
+        outFile: 'global.css',
+        includePaths: [Path.resolve('src')]
+    });
 
-// console.log(result.css.toString());
+    Fs.writeFileSync(
+        Path.resolve(fileName), // 'src/lib/global.css'
+        result.css.toString()
+    )
+}
 
-Fs.writeFileSync(
-    Path.resolve('src/lib/global.css'),
-    result.css.toString()
-)
+compile('src/global.scss', 'src/lib/global.css');
