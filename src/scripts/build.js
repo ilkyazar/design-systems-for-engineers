b64d2b0d21a515aac19c7d37378e9aecd31f8829
ilkyazarabaci <ilkyaz.arabaci@adesso.com.tr>
2023-10-02 21:35:56 +0300

    feat: compile components to css

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -2,20 +2,47 @@ const Fs = require('fs')
 const Path = require('path')
 const Sass = require('node-sass')
 
+const getComponents = () => {
+    let allComponents = []
+
+    const types = ['atoms', 'molecules', 'organisms']
+
+    types.forEach(type => {
+        const allFiles = Fs.readdirSync(`src/${type}`).map(file => ({
+            input: `src/${type}/${file}`,
+            output: `lib/${file.slice(0, -4) + 'css'}`
+        }))
+
+        allComponents = [
+            ...allComponents,
+            ...allFiles
+        ]
+    })
+
+    return allComponents
+}
+
 const compile = (path, fileName) => {
     const result = Sass.renderSync({
         data: Fs.readFileSync(
             Path.resolve(path) // 'src/global.scss'
         ).toString(),
         outputStyle: 'expanded',
-        outFile: 'global.css',
         includePaths: [Path.resolve('src')]
     });
 
     Fs.writeFileSync(
         Path.resolve(fileName), // 'src/lib/global.css'
         result.css.toString()
-    )
+    );
 }
 
-compile('src/global.scss', 'src/lib/global.css');
+try {
+    Fs.mkdirSync(Path.resolve('lib'))
+} catch(e) {}
+
+compile('src/global.scss', 'lib/global.css');
+
+getComponents().forEach(component => {
+    compile(component.input, component.output)
+});
