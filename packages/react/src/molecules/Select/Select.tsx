fc62871ec2ba1df458858ea01471883335823dd1
ilkyazarabaci <ilkyaz.arabaci@adesso.com.tr>
2023-10-08 19:30:43 +0300

    feat: animate select caret

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -47,8 +47,9 @@ const Select: React.FC<SelectProps> = ({ options = [], label='Please select an o
     return <div className="dse-select">
         <button ref={labelRef} className="dse-select__label" onClick={() => onLabelClick()}>
             <Text>{selectedOption === null ? label : selectedOption.label}</Text>
-            <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" 
-                viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} 
+                width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" 
+                viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
         </button>
