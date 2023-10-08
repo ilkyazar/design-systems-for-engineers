4b6b1e5f2e7e604eedb30f40db9ff96ab5460239
ilkyazarabaci <ilkyaz.arabaci@adesso.com.tr>
2023-10-08 18:54:20 +0300

    feat: calculate the overlay position for select

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface SelectOption {
     label: string,
@@ -12,7 +12,10 @@ interface SelectProps {
 }
 
 const Select: React.FC<SelectProps> = ({ options = [], label='Please select an option', onOptionSelected: handler }) => {
-    
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const labelRef = useRef<HTMLButtonElement>(null);
+    const [overlayTop, setOverlayTop] = useState<number>(0);
+
     const onOptionSelected = (option: SelectOption, optionIndex: number) => {
         setIsOpen(!isOpen);
         
@@ -21,14 +24,18 @@ const Select: React.FC<SelectProps> = ({ options = [], label='Please select an o
         }
     }
 
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-
     const onLabelClick = () => {
         setIsOpen(!isOpen);
     }
 
+    useEffect(() => {
+        setOverlayTop((
+            labelRef.current?.offsetHeight || 0
+        ) + 10)
+    }, [labelRef.current?.offsetHeight])
+
     return <div className="dse-select">
-        <button className="dse-select__label" onClick={() => onLabelClick()}>
+        <button ref={labelRef} className="dse-select__label" onClick={() => onLabelClick()}>
             <span>{label}</span>
             <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -37,7 +44,7 @@ const Select: React.FC<SelectProps> = ({ options = [], label='Please select an o
         </button>
 
         {isOpen ? (
-            <ul className="dse-select__overlay">
+            <ul style={{ top: overlayTop }} className="dse-select__overlay">
             {options.map((option, optionIndex) => {
                 return <li key={option.value} onClick={() => onOptionSelected(option, optionIndex)}>{option.label}</li>
             })}
