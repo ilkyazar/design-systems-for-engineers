4281b3c76a1ab240b783550a7392a131168e5850
ilkyazarabaci <ilkyaz.arabaci@adesso.com.tr>
2023-10-08 17:16:24 +0300

    feat: add select styles

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -27,13 +27,17 @@ const Select: React.FC<SelectProps> = ({ options = [], label='Please select an o
         setIsOpen(!isOpen);
     }
 
-    return <div>
-        <button onClick={() => onLabelClick()}>
-            {label}
+    return <div className="dse-select">
+        <button className="dse-select__label" onClick={() => onLabelClick()}>
+            <span>{label}</span>
+            <svg width="1rem" height="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+</svg>
+
         </button>
 
         {isOpen ? (
-            <ul>
+            <ul className="dse-select__overlay">
             {options.map((option, optionIndex) => {
                 return <li key={option.value} onClick={() => onOptionSelected(option, optionIndex)}>{option.label}</li>
             })}
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -7,6 +7,7 @@ import '@ds.e/scss/lib/Button.css';
 import '@ds.e/scss/lib/Utilities.css';
 import '@ds.e/scss/lib/Text.css';
 import '@ds.e/scss/lib/Margin.css';
+import '@ds.e/scss/lib/Select.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
